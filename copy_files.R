55ff704aaf148bd4ff525f615fc3fae3bdb47b58
Alex <ailich@usf.edu>
2024-12-12 23:24:32 +0000

    Fix typos in sonar name - Fix typos in sonar names - Comit server - Remove perl=TRUE in regex b/c no longer works

--- copy_files.R
+++ copy_files.R
@@ -3,10 +3,14 @@ library(tidyverse)
 
 terraOptions(datatype="FLT8S")
 
-x<- read_csv("Z://2_Projects/GIS/Multibeam_Metadata_And_Footprints_AI/Multibeam_metadata.csv")
+x<- read_csv("Multibeam_metadata.csv")
 x<- x %>% filter(!is.na(Server_Location))
 
-x<- x %>% mutate(Server_Location= if_else(str_detect(Server_Location, "^D:"), Server_Location, paste0("Z:/", Server_Location)))
+unique(str_extract(x$Filename, "\\..+$"))
+
+x<- x %>% mutate(Server_Location = case_when(str_detect(Server_Location, "^NFWF/") ~ str_replace(Server_Location, "^NFWF/", "Z://"),
+                                         str_detect(Server_Location, "^comit_wrk/") ~ str_replace(Server_Location, "^comit_wrk/", "Y://"),
+                                         TRUE ~ Server_Location))
 
 x<- x %>% mutate(NewFilename=paste0(paste(formatC(ID, width = 3, format = "d", flag = "0"), Site, Subsite,Type, Source, sep = "_"), ".tif"))
 
